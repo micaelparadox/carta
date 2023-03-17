56bd2903a665fa25ec11be373f60bcafc2bfb200
MICAEL SANTANA <micael.santana@t-systems.com>
2023-03-17 14:27:12 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    // Animar a saudação
+    $(".div-topo").addClass("animate-left");
+
+    // Animar a mensagem
+    $(".mensagem").addClass("animate-right");
+});
